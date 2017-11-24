3cf325dbf8d01c5afffee293c2c76b085365d640
Frank Lemanschik <frank@github.dspeed.eu>
2017-11-24 21:13:30 +0000

    Update vm.js

--- component/templates/modlet/vm.js
+++ component/templates/modlet/vm.js
@@ -1,7 +1,8 @@
 import DefineMap from 'can-define/map/';
-
 export const ViewModel = DefineMap.extend({
   message: {
     value: 'This is the <%= tag %> component'
   }
 });
+
+export default ViewModel
